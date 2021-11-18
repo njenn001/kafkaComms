ccec43d15e09f78946d29086b3cadf4d9e27b012
njenning <njenning@cs.odu.edu>
2021-11-17 23:54:21 -0500

    Requirement install 0.1.7

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+import os 
 
 from objects.user import User
+
+# Install dependencies before execution 
+def satisfy_requirements(): 
+    try: 
+        os.system('pip install -r requirements.txt')
+    except Exception as ex: 
+        print(ex) 
+        os.sys.exit() 
 
 def app(): 
 
@@ -7,4 +16,5 @@ def app():
     you.run() 
 
 if __name__ == '__main__': 
+    satisfy_requirements() 
     app() 
